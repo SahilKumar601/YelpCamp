54c6c09d4a3dca386b04a5928789e448b94243f7
Sahil Kumar <sahilku601@gmail.com>
2023-06-01 18:47:46 +0530

    -Added Basic Styles and Images to DB and in /show.ejs

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const app = express();
 const path = require("path");
+const ejsMate=require('ejs-mate')
 const mongoose = require("mongoose");
 const Campground = require("./models/campgrounds");
 const methodOverride = require("method-override");
@@ -14,6 +15,7 @@ mongoose
     console.log(err.message);
 });
 
+app.engine('ejs',ejsMate)
 app.set("view enigne", "ejs");
 app.set("views", path.join(__dirname, "views"));
 app.use(express.urlencoded({ extended: true }));
--- seeds/index.js
+++ seeds/index.js
@@ -16,13 +16,18 @@ const seedDb =async()=>{
     await Campground.deleteMany({});
     for (let i = 0; i < 50; i++) {
         const random=Math.floor(Math.random()*1000);
+        const price=Math.floor(Math.random()*20)+10
         const camp=new Campground({
             location:`${cities[random].city}, ${cities[random].state}`,
-            title:`${sample(descriptors)} ${sample(places)}`
+            title:`${sample(descriptors)} ${sample(places)}`,
+            image:'https://source.unsplash.com/collection/483251',
+            description:'Lorem ipsum, dolor sit amet consectetur adipisicing elit. Atque, voluptas. Excepturi autem corrupti aspernatur eaque iusto est saepe delectus velit placeat animi. Eius quasi ut porro laudantium laboriosam cumque provident!',
+            price
         })
         await camp.save();
     }
 }
 seedDb().then(()=>{
     mongoose.connection.close();
+    console.log('Data Saved!! closing')
 })
